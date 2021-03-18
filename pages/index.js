69e6b04bf9cd54458d375e09785859e5f39d9ea7
Stijn Coolen <stijn@wijzijnmiddag.nl>
2021-03-18 20:09:16 +0100

    Added page structure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Layout from '../components/layout'
+import Link from 'next/link'
+
 import { getAllPeople, getAllPlanets, getAllStarShips } from './api/data'
 
 export async function getStaticProps() {
@@ -14,23 +16,38 @@ export async function getStaticProps() {
 export default function Home({people, planets, starships}) {
   return (
     <Layout>
-      <ul>
-        {people.map((person) => (
-          <li>{person.name}</li>
-        ))}
-      </ul>
-
-      <ul>
-        {planets.map((person) => (
-          <li>{person.name}</li>
-        ))}
-      </ul>
+      <section>
+        <h2>Popular Starships</h2>
+        <ul>
+          {starships.map((ship) => (
+            <li>{ship.name}</li>
+          ))}
+        </ul>
+        <Link href="/starships">
+          <a>View more</a>
+        </Link>
+      </section>
       
-      <ul>
-        {starships.map((person) => (
-          <li>{person.name}</li>
-        ))}
-      </ul>
+      <section>
+        <h2>Popular Planets</h2>
+        <ul>
+          {planets.map((planet) => (
+            <li>{planet.name}</li>
+          ))}
+        </ul>
+      </section>
+      
+      <section>
+        <h2>Popular Characters</h2>
+        <ul>
+          {people.map((person) => (
+            <li>{person.name}</li>
+          ))}
+        </ul>
+      </section>
+      <Link href="/characters">
+          <a>View more</a>
+        </Link>
     </Layout>
   )
 }
